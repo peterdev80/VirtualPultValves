1aac23f02b07eed932466188e7051e287af87cd5
gbdev <peter.dev@live.com>
2018-01-16 15:53:14 +0300

    mmm

---
+++ Malicow/VirtualNeptun/PultNeptun/PultNeptun/WindowInpu.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PultNeptun
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowInpu.xaml
+    /// </summary>
+    public partial class WindowInpu : UserControl
+    {
+        public WindowInpu()
+        {
+            InitializeComponent();
+        }
+        private void PultGlassButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void PultGlassButton_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+
+
+        }
+    }
+}
